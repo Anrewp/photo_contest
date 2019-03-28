2ed0eaa2a9a38e3e21ce0e788b8ba247a0434d18
Anrewp <anrewp1991@gmail.com>
2019-03-28 12:30:41 +0300

    omniauth.rb fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :vkontakte, ENV['VKONTAKTE_KEY'], ENV['VKONTAKTE_SECRET'], scope: "email"
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] scope: "user:email"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email"
 
 end
