1ebed2498270c91517537a1d2c04eab00e5d0b38
Anrewp <anrewp1991@gmail.com>
2019-05-03 18:19:28 +0300

    fix3

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -18,7 +18,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
