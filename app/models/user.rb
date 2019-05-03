de5aafc127daba554a7a4bd1d3e45275e18c0a57
Anrewp <anrewp1991@gmail.com>
2019-05-03 14:23:29 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   def User.from_omniauth(auth_hash)
     name = auth_hash['provider'] == 'vkontakte' ? "name" : "nickname"
     user = find_or_create_by(email: auth_hash.info.email, provider: auth_hash.provider)
-    if user.name.empty?
+    if user.name.blank?
      user.name = auth_hash.info.public_send(name)
      user.image_url = auth_hash.info.image
      user.token = auth_hash.credentials.token
